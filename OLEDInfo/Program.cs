83067365b3dca00e57a42583d49932cf26ff565f
Paweł Kania <pawel.kania@onet.pl>
2019-04-12 21:09:28 +0200

    Add new screen types

--- OLEDInfo/Program.cs
+++ OLEDInfo/Program.cs
@@ -18,6 +18,11 @@ namespace Tivian.Display {
         private static readonly byte displayAddr = 0x3c;
         private static readonly PrivateFontCollection fontCollection = new PrivateFontCollection();
 
+        private static readonly float fontSize = 20f;
+        private static readonly Font font = new Font("Consolas", fontSize);
+        private static readonly Pen pen = new Pen(Color.White);
+        private static readonly int defaultDelay = 1000;
+
         private static OLED disp;
         private static Computer computer;
         private static CancellationTokenSource tokenSrc;
@@ -32,23 +37,31 @@ namespace Tivian.Display {
             fontCollection.AddResourceFont(Properties.Resources._7_segment_mono);
 
             computer = new Computer() {
-                GPUEnabled = true,
-                CPUEnabled = true
+                CPUEnabled = true,
+                RAMEnabled = true,
+                MainboardEnabled = true,
+                GPUEnabled = true
             };
             computer.Open();
 
-            float fontSize = 20.0f;
-            var font = new Font("Consolas", fontSize);
-            var pen = new Pen(Color.White);
+            Action<Graphics, string[], Func<string, string>> printSensors = (g, labels, value) => {
+                var baseLine = 0.0f;
+                var smallFont = new Font(font.FontFamily, font.Size / 2.5f);
+                string text = string.Empty;
+                SizeF measure = SizeF.Empty;
 
-            var families = FontFamily.Families.Where(family => {
-                var g = Graphics.FromImage(new Bitmap(100, 100));
-                return g.MeasureString("000.00", new Font(family, 100)).Width - g.MeasureString("  1.11", new Font(family, 100)).Width == 0;
-            }).ToArray();
+                foreach (var label in labels) {
+                    g.DrawString(label, smallFont, pen.Brush, 0, baseLine);
+                    text = value(label);
+                    measure = g.MeasureString(text, font);
+                    g.DrawString(text, font, pen.Brush, disp.Width - measure.Width - 5, baseLine + smallFont.Size * 0.5f);
+                    baseLine += font.Size * 1.6f;
+                }
+            };
 
             int current = 0;
             var screens = new List<Screen>() {
-                new Screen() {
+                new Screen() { // current time
                     Delay = 30000,
                     Draw = (g) => {
                         var timeFont = new Font(fontCollection.Families[0], font.Size * 1.5f);
@@ -58,40 +71,57 @@ namespace Tivian.Display {
                     }
                 },
 
-                new Screen() {
-                    Delay = 1000,
-                    Draw = (g) => {
-                        var baseLine = 0.0f;
-                        var smallFont = new Font(font.FontFamily, font.Size / 2.5f);
-                        var cpu = computer.Hardware.Where(h => h.HardwareType == HardwareType.CPU).FirstOrDefault();
-                        cpu.Update();
-                        g.DrawString("CPU", smallFont, pen.Brush, 0, baseLine);
-                        g.DrawString($"{cpu.Sensors.Where(s => s.SensorType == SensorType.Power).FirstOrDefault().Value,6:f2}W", font, pen.Brush, 10, baseLine + smallFont.Size * 0.5f);
-                        baseLine += font.Size * 1.6f;
-
-                        var gpu = computer.Hardware.Where(h => h.HardwareType == HardwareType.GpuNvidia).FirstOrDefault();
-                        gpu.Update();
-                        g.DrawString("GPU", smallFont, pen.Brush, 0, baseLine);
-                        g.DrawString($"{gpu.Sensors.Where(s => s.SensorType == SensorType.Power).FirstOrDefault().Value,6:f2}W", font, pen.Brush, 10, baseLine + smallFont.Size * 0.5f);
-                    }
+                new Screen() { // CPU / GPU power consumption
+                    Delay = defaultDelay,
+                    Draw = (g) => printSensors(g, new[] { "CPU", "GPU" }, (label) => {
+                        var hardware = computer.Hardware.Where(h => h.HardwareType == ((label == "CPU") ? HardwareType.CPU : HardwareType.GpuNvidia)).FirstOrDefault();
+                        hardware.Update();
+                        return $"{hardware.Sensors.Where(s => s.SensorType == SensorType.Power).FirstOrDefault().Value,6:f2}W";
+                    })
                 },
 
-                new Screen() {
-                    Delay = 1000,
-                    Draw = (g) => {
-                        var baseLine = 0.0f;
-                        var smallFont = new Font(font.FontFamily, font.Size / 2.5f);
-                        var cpu = computer.Hardware.Where(h => h.HardwareType == HardwareType.CPU).FirstOrDefault();
-                        cpu.Update();
-                        g.DrawString("CPU", smallFont, pen.Brush, 0, baseLine);
-                        g.DrawString($"{cpu.Sensors.Where(s => s.SensorType == SensorType.Temperature).FirstOrDefault().Value,2:f0}℃", font, pen.Brush, 10, baseLine + smallFont.Size * 0.5f);
-                        baseLine += font.Size * 1.6f;
+                new Screen() { // CPU / GPU temperature
+                    Delay = defaultDelay,
+                    Draw = (g) => printSensors(g, new[] { "CPU", "GPU" }, (label) => {
+                        var hardware = computer.Hardware.Where(h => h.HardwareType == ((label == "CPU") ? HardwareType.CPU : HardwareType.GpuNvidia)).FirstOrDefault();
+                        hardware.Update();
+                        return $"{hardware.Sensors.Where(s => s.SensorType == SensorType.Temperature).LastOrDefault().Value,2:f0}℃";
+                    })
+                },
 
-                        var gpu = computer.Hardware.Where(h => h.HardwareType == HardwareType.GpuNvidia).FirstOrDefault();
-                        gpu.Update();
-                        g.DrawString("GPU", smallFont, pen.Brush, 0, baseLine);
-                        g.DrawString($"{gpu.Sensors.Where(s => s.SensorType == SensorType.Temperature).FirstOrDefault().Value,2:f0}℃", font, pen.Brush, 10, baseLine + smallFont.Size * 0.5f);
-                    }
+                new Screen() { // CPU / GPU load level
+                    Delay = defaultDelay,
+                    Draw = (g) => printSensors(g, new[] { "CPU", "GPU" }, (label) => {
+                        string name = (label == "CPU") ? "Total" : "Core";
+                        var hardware = computer.Hardware.Where(h => h.HardwareType == ((label == "CPU") ? HardwareType.CPU : HardwareType.GpuNvidia)).FirstOrDefault();
+                        hardware.Update();
+                        return $"{hardware.Sensors.Where(s => s.SensorType == SensorType.Load && s.Name.Contains(name)).FirstOrDefault().Value,4:f2}%";
+                    })
+                },
+
+                new Screen() { // RAM / VRAM usage
+                    Delay = defaultDelay,
+                    Draw = (g) => printSensors(g, new[] { "RAM             [MB]", "Video RAM       [MB]" }, (label) => {
+                        float factor = label.StartsWith("RAM") ? 1000f : 1f;
+                        var type = label.StartsWith("RAM") ? HardwareType.RAM : HardwareType.GpuNvidia;
+                        var hardware = computer.Hardware.Where(h => h.HardwareType == type).FirstOrDefault();
+                        hardware.Update();
+                        return $"{hardware.Sensors.Where(s => s.Name.Contains("Used")).FirstOrDefault().Value * factor,8:f0}";
+                    })
+                },
+
+                new Screen() { // CPU / GPU cooler speed
+                    Delay = defaultDelay,
+                    Draw = (g) => printSensors(g, new[] { "CPU cooler     [RPM]", "GPU fan" }, (label) => {
+                        var unit = label.Contains("CPU") ? "" : "%";
+                        var type = label.Contains("CPU") ? HardwareType.Mainboard : HardwareType.GpuNvidia;
+                        var sensorType = label.Contains("CPU") ? SensorType.Fan : SensorType.Level;
+                        var hardware = computer.Hardware.Where(h => h.HardwareType == type).FirstOrDefault();
+                        if (label.Contains("CPU"))
+                            hardware = hardware.SubHardware[0];
+                        hardware.Update();
+                        return $"{hardware.Sensors.Where(s => s.SensorType == sensorType).FirstOrDefault().Value,6:f1}{unit}";
+                    })
                 },
 
                 new Screen() {
@@ -101,17 +131,10 @@ namespace Tivian.Display {
             };
 
             KeyLogg.Instance.OnKeyDown += (sender, e) => {
-                bool cancel = true;
-                if (e.Key == Keys.RWin && e.ModifierKeys == Keys.Control) {
+                if (e.Key == Keys.F11 && e.ModifierKeys.HasFlag(Keys.Control)) {
                     if (++current == screens.Count)
                         current = 0;
-                } else if (e.Key == Keys.F11 && e.ModifierKeys.HasFlag(Keys.Control) && e.ModifierKeys.HasFlag(Keys.Alt)) {
-                    isRunning = false;
-                } else {
-                    cancel = false;
-                }
 
-                if (cancel) {
                     try {
                         tokenSrc?.Cancel();
                     } catch (ObjectDisposedException) { }
@@ -121,9 +144,11 @@ namespace Tivian.Display {
             using (disp = new OLED(new UART(diplayPort, baudrate, displayAddr)))
             using (var img = new Bitmap(disp.Width, disp.Height))
             using (var g = Graphics.FromImage(img)) {
+                g.TextRenderingHint = TextRenderingHint.SingleBitPerPixelGridFit;
+                Application.ApplicationExit += (sender, e) => disp.Dispose();
+
                 while (isRunning) {
                     g.Clear(Color.Black);
-
                     screens[current].Draw(g);
                     disp.Display(img);
 
